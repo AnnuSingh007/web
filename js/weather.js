cbc98fa17ee93f642df792a4a892d2f35bf2e68c
AnnuSingh007 <81318314+AnnuSingh007@users.noreply.github.com>
2021-10-14 19:11:15 +0530

    Initial commit

---
+++ js/weather.js
@@ -0,0 +1,26 @@
+    let placeSearch;
+    let autocomplete;
+    const componentForm = {
+        street_number: "short_name",
+        route: "long_name",
+        sublocality_level_1: "long_name",
+        locality: "long_name",
+        administrative_area_level_1: "short_name",
+        country: "long_name",
+        postal_code: "short_name",
+    };
+
+    function initAutocomplete() {
+        // Create the autocomplete object, restricting the search predictions to
+        // geographical location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            document.getElementById("enter-city"),
+            { types: ["geocode"] }
+        );
+        // Avoid paying for data that you don't need by restricting the set of
+        // place fields that are returned to just the address components.
+        autocomplete.setFields(["address_component"]);
+        // When the user selects an address from the drop-down, populate the
+        // address fields in the form.
+        autocomplete.addListener("place_changed", fillInAddress);
+    }
